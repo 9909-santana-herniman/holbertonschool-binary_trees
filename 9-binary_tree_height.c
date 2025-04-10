d492c8d8e47a03d245c565194de2ee361dc26173
9909-santana-herniman <santana.herniman@gmail.com>
2025-04-10 23:41:23 +0000

    Write a function that measures the height of a binary tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,6 +1,28 @@
 #include "binary_trees.h"
 
 /**
+ * calculate_subtree_height - Calculates the height of a subtree.
+ * @subtree: Pointer to the subtree to measure.
+ * Return: The height of the subtree.
+ */
+
+size_t calculate_subtree_height(const binary_tree_t *subtree)
+{
+	/* Check if subtree exists */
+	if (subtree != NULL)
+	{
+		/* Calculate the height of the subtree */
+		/* The height is 1 (for current node */
+		/* Plus the height of the subtree below it */
+		return (1 + binary_tree_height(subtree));
+	}
+	else
+	{
+		return (0);
+	}
+}
+
+/**
  * binary_tree_height - Measures the height of a binary tree.
  * @tree: Pointer to the root node of the tree to measure the height.
  * Return: The height of the tree. If tree is NULL, return 0.
@@ -17,14 +39,18 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree->left == NULL && tree->right == NULL)
 		return (0);
 
-	/* Recursively calculate the height of the left and right subtrees */
-	left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	/* Calculate the height of the left & right subtree*/
+	left_height = calculate_subtree_height(tree->left);
+	right_height = calculate_subtree_height(tree->right);
 
-	/* The height of each subtree is 1 (for the current node) plus */
-	/* the height of its subtree. If a subtree is NULL, its height */
-	/* is considered 0 */
-
-	/* The height of the current node is the maximum height of its subtrees */
-	return (left_height > right_height ? left_height : right_height);
+	/* The height of the current node is the maximum */
+	/*  height of its subtrees */
+	if (left_height > right_height)
+	{
+		return (left_height); /* Left is higher */
+	}
+	else
+	{
+		return (right_height); /* Right is higher or equal */
+	}
 }
