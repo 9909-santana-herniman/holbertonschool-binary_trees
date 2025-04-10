09092139af9ff9ad85a2df1774286d9a133cc43d
9909-santana-herniman <santana.herniman@gmail.com>
2025-04-10 22:11:57 +0000

    Write a function that checks if a node is a lear

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - Checks if a node is a leaf.
+ * @node: Pointer to the node to check.
+ * Return: 1 if node is a leaf, otherwise 0.
+ * If node is NULL, return 0.
+ */
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	/* Check if the input node is NULL */
+	if (node == NULL)
+		return (0);
+
+	/* A node is a leaf if both its left and right children are NULL */
+	if (node->left == NULL && node->right == NULL)
+		return (1);
+
+	/* Otherwise, it's not a leaf */
+	return (0);
+}
