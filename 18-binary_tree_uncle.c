164b3862c4e2482540463fe4cef4bf677141ce42
9909-santana-herniman <santana.herniman@gmail.com>
2025-04-12 02:51:58 +0000

    Write a function that finds the uncle of a node

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -16,20 +16,27 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 		return (NULL);
 
 	/* If the node has no parent, it can't have an uncle */
+	if (node->parent == NULL)
+		return (NULL);
+
+	/* If the node has no parent, it can't have an uncle */
 	if (node->parent->parent == NULL)
 		return (NULL);
 
 	/* Get a pointer to the grand parent node */
 	grandparent = node->parent->parent;
 
+	if (grandparent == NULL)
+		return (NULL);
+
 	/* If the parent is the left child of the grandparent, the */
 	/* uncle is the right child */
-	if (node->parent == grandparent->left)
+	if (grandparent->left != NULL && node->parent == grandparent->left)
 		return (grandparent->right);
 
 	/* If the parent is the right child of the grandparent, the */
 	/* uncle is the left child */
-	if (node->parent == grandparent->right)
+	if (grandparent->right != NULL && node->parent == grandparent->right)
 		return (grandparent->left);
 
 	/* If none of the above conditions are met, there is no uncle */
